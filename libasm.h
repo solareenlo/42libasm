27545c8064f685a9cea7de6ffebd1b1d48da14af
solareenlo <solareenlo@gmail.com>
2021-02-21 16:32:59 +0900

    add: libasm.h

---
+++ libasm.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libasm.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/21 14:32:53 by tayamamo          #+#    #+#             */
+/*   Updated: 2021/02/21 16:24:40 by tayamamo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	LIBASM_H
+# define LIBASM_H
+
+# include <stddef.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strcpy(char *dst, const char *src);
+int		ft_strcmp(const char *s1, const char *s2);
+ssize_t	ft_write(int fildes, const void *buf, size_t nbyte);
+ssize_t	ft_read(int fildes, void *buf, size_t nbyte);
+char	*ft_strdup(const char *s1);
+
+#endif
