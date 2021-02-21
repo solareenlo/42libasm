08c2f06529a26a1a108c39b3bd4447c97ad3ee5a
solareenlo <solareenlo@gmail.com>
2021-02-22 02:01:01 +0900

    add: ft_write

---
+++ ft_write.s
@@ -0,0 +1,26 @@
+; **************************************************************************** ;
+;                                                                              ;
+;                                                         :::      ::::::::    ;
+;    ft_write.s                                         :+:      :+:    :+:    ;
+;                                                     +:+ +:+         +:+      ;
+;    By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+         ;
+;                                                 +#+#+#+#+#+   +#+            ;
+;    Created: 2021/02/22 00:35:58 by tayamamo          #+#    #+#              ;
+;    Updated: 2021/02/22 00:35:58 by tayamamo         ###   ########.fr        ;
+;                                                                              ;
+; **************************************************************************** ;
+
+			global	_ft_write
+			section	.text
+			extern	___error
+_ft_write:
+			mov		rax, 0x2000004
+			syscall
+			jc		.error
+			ret
+.error:
+			push	rax
+			call	___error
+			pop		qword [rax]
+			mov		rax, -1
+			ret
