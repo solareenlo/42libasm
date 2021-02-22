a5f4c723bb49628365ad6097e6edfe11a4a40a41
solareenlo <solareenlo@gmail.com>
2021-02-23 04:45:58 +0900

    fix

--- ft_strdup.s
+++ ft_strdup.s
@@ -10,29 +10,40 @@
 ;                                                                              ;
 ; **************************************************************************** ;
 
+; size_t	ft_strlen(const char *s);
+; char		*ft_strcpy(char *dst, const char *src);
+; char		*ft_strdup(const char *s1);
+
 %ifdef MACOS
 	%define FT_STRDUP	_ft_strdup
 	%define FT_STRCPY	_ft_strcpy
 	%define FT_STRLEN	_ft_strlen
 	%define MALLOC		_malloc
+	%define ERROR		___error
 %else
 	%define	FT_STRDUP	ft_strdup
 	%define	FT_STRCPY	ft_strcpy
 	%define	FT_STRLEN	ft_strlen
 	%define MALLOC		malloc
+	%define ERROR		__errno_location
 %endif
 
 			global	FT_STRDUP
 			extern	FT_STRLEN
 			extern	FT_STRCPY
 			extern	MALLOC
+			extern	ERROR
 			section	.text
 FT_STRDUP:
 			call	FT_STRLEN
 			inc		rax
 			push	rdi
 			mov		rdi, rax
+%ifdef MACOS
 			call	MALLOC
+%else
+			call	MALLOC wrt ..plt
+%endif
 			test	rax, rax
 			jz		.error
 			pop		rsi
@@ -40,5 +51,16 @@ FT_STRDUP:
 			call	FT_STRCPY
 			ret
 .error:
+%ifndef MACOS
+			neg		rax
+%endif
 			pop		rdi
+			push	rax
+%ifdef MACOS
+			call	ERROR
+%else
+			call	ERROR wrt ..plt
+%endif
+			pop		qword [rax]
+			mov		rax, 0
 			ret
