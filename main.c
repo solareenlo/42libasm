b200b61b26e7716be3089b006ee84eb0afd9519e
solareenlo <solareenlo@gmail.com>
2021-02-23 05:11:28 +0900

    fix

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 15:01:09 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/02/23 05:00:38 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/02/23 05:10:48 by tayamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,40 +23,19 @@
 #define RED "\033[31m"
 #define GREEN "\033[32m"
 
-void	test_ft_strlen(void);
-void	check_ft_strlen(char *s);
-void	test_ft_strcpy(void);
-void	check_ft_strcpy(char *src);
-void	test_ft_strcmp(void);
-void	check_ft_strcmp(char *s1, char *s2);
-void	test_ft_write(void);
-void	check_ft_write(int fd, const void *buf, size_t n);
-void	test_ft_read(void);
-void	check_ft_read(char *filename, size_t n);
-void	test_ft_strdup(void);
-void	check_ft_strdup(char *s);
-char	*long_char(size_t size, char c);
-
-int		main(void)
+char	*long_char(size_t size, char c)
 {
-	test_ft_strlen();
-	test_ft_strcpy();
-	test_ft_strcmp();
-	test_ft_write();
-	test_ft_read();
-	test_ft_strdup();
-	return (0);
-}
+	char	*s;
 
-void	test_ft_strdup(void)
-{
-	printf("%sCheck%s ft_strdup\n", GREEN, RESET);
-	check_ft_strdup("hello world");
-	check_ft_strdup("");
-	check_ft_strdup("     ");
-	check_ft_strdup("\0\0\0");
-	check_ft_strdup("\0a\0");
-	check_ft_strdup("a\0a\0");
+	if ((s = malloc(size)) == NULL)
+	{
+		dprintf(2, "malloc failed\n");
+		exit(1);
+	}
+	s[--size] = '\0';
+	while (--size)
+		s[size] = c;
+	return (s);
 }
 
 void	check_ft_strdup(char *s)
@@ -79,25 +58,15 @@ void	check_ft_strdup(char *s)
 	free(dup[1]);
 }
 
-void	test_ft_read(void)
+void	test_ft_strdup(void)
 {
-	printf("%sCheck%s ft_read\n", GREEN, RESET);
-	check_ft_read("a.txt", 10);
-	check_ft_read("b.txt", 10);
-	check_ft_read(NULL, 10);
-	check_ft_read("", 10);
-	check_ft_read("a.txt", 0);
-	check_ft_read("b.txt", 0);
-	check_ft_read(NULL, 0);
-	check_ft_read("", 0);
-	check_ft_read("a.txt", -1);
-	check_ft_read("b.txt", -1);
-	check_ft_read(NULL, -1);
-	check_ft_read("", -1);
-	check_ft_read("a.txt", 1024);
-	check_ft_read("b.txt", 1024);
-	check_ft_read(NULL, 1024);
-	check_ft_read("", 1024);
+	printf("%sCheck%s ft_strdup\n", GREEN, RESET);
+	check_ft_strdup("hello world");
+	check_ft_strdup("");
+	check_ft_strdup("     ");
+	check_ft_strdup("\0\0\0");
+	check_ft_strdup("\0a\0");
+	check_ft_strdup("a\0a\0");
 }
 
 void	check_ft_read(char *filename, size_t n)
@@ -124,6 +93,37 @@ void	check_ft_read(char *filename, size_t n)
 	else
 		printf("%sNG%s [%s] [%s] [%lu] [%lu] [%d]\n",
 				RED, RESET, filename, buf[0], n, ret[0], errnum[1]);
+}
+
+void	test_ft_read(void)
+{
+	printf("%sCheck%s ft_read\n", GREEN, RESET);
+	check_ft_read("a.txt", 10);
+	check_ft_read("b.txt", 10);
+	check_ft_read(NULL, 10);
+	check_ft_read("", 10);
+	check_ft_read("a.txt", 0);
+	check_ft_read("b.txt", 0);
+	check_ft_read(NULL, 0);
+	check_ft_read("", 0);
+	check_ft_read("a.txt", -1);
+	check_ft_read("b.txt", -1);
+	check_ft_read(NULL, -1);
+	check_ft_read("", -1);
+	check_ft_read("a.txt", 1024);
+	check_ft_read("b.txt", 1024);
+	check_ft_read(NULL, 1024);
+	check_ft_read("", 1024);
+}
+
+void	check_ft_write(int fd, const void *buf, size_t n)
+{
+	errno = 0;
+	printf("[%lu] ", write(fd, buf, n));
+	printf("[%d]\n", errno);
+	errno = 0;
+	printf("[%lu] ", ft_write(fd, buf, n));
+	printf("[%d]\n", errno);
 }
 
 void	test_ft_write(void)
@@ -153,14 +153,15 @@ void	test_ft_write(void)
 	free(s);
 }
 
-void	check_ft_write(int fd, const void *buf, size_t n)
+void	check_ft_strcmp(char *s1, char *s2)
 {
-	errno = 0;
-	printf("[%lu] ", write(fd, buf, n));
-	printf("[%d]\n", errno);
-	errno = 0;
-	printf("[%lu] ", ft_write(fd, buf, n));
-	printf("[%d]\n", errno);
+	if (strcmp(s1, s2) == ft_strcmp(s1, s2))
+		printf("%sOK%s [%s] [%s]\n", GREEN, RESET, s1, s2);
+	else
+	{
+		printf("%sNG%s [%s] [%s]", RED, RESET, s1, s2);
+		printf(" %d %d\n", strcmp(s1, s2), ft_strcmp(s1, s2));
+	}
 }
 
 void	test_ft_strcmp(void)
@@ -182,43 +183,6 @@ void	test_ft_strcmp(void)
 	s = long_char(100, 'C');
 	check_ft_strcmp(s, "a");
 	check_ft_strcmp(s, s);
-	free(s);
-	write(1, "\n", 1);
-}
-
-void	check_ft_strcmp(char *s1, char *s2)
-{
-	if (strcmp(s1, s2) == ft_strcmp(s1, s2))
-		printf("%sOK%s [%s] [%s]\n", GREEN, RESET, s1, s2);
-	else
-	{
-		printf("%sNG%s [%s] [%s]", RED, RESET, s1, s2);
-		printf(" %d %d\n", strcmp(s1, s2), ft_strcmp(s1, s2));
-	}
-}
-
-void	test_ft_strcpy(void)
-{
-	char	*s;
-
-	printf("%sCheck%s ft_strcpy\n", GREEN, RESET);
-	check_ft_strcpy("hello world");
-	check_ft_strcpy("");
-	check_ft_strcpy("          ");
-	check_ft_strcpy(" a ");
-	check_ft_strcpy("0x 0 0 \' \' \"\" ");
-	check_ft_strcpy("\0\0\0");
-	check_ft_strcpy("\n\n");
-	check_ft_strcpy("a\0b\0c\0");
-	check_ft_strcpy("!@#$%^&*()_+|~QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>?");
-	check_ft_strcpy(":\"A");
-	check_ft_strcpy("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-	check_ft_strcpy("!@#$%^&*()_+|~\"QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>?");
-	check_ft_strcpy("!@#$%^&*()_+|~\"QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?");
-	check_ft_strcpy("\'\'\'\'\'\'\'\'");
-	check_ft_strcpy("\"\"\"\"\"\"\"\"");
-	s = long_char(100, 'A');
-	check_ft_strcpy(s);
 	free(s);
 	write(1, "\n", 1);
 }
@@ -248,6 +212,40 @@ void	check_ft_strcpy(char *src)
 	free(dst2);
 }
 
+void	test_ft_strcpy(void)
+{
+	char	*s;
+
+	printf("%sCheck%s ft_strcpy\n", GREEN, RESET);
+	check_ft_strcpy("hello world");
+	check_ft_strcpy("");
+	check_ft_strcpy("          ");
+	check_ft_strcpy(" a ");
+	check_ft_strcpy("0x 0 0 \' \' \"\" ");
+	check_ft_strcpy("\0\0\0");
+	check_ft_strcpy("\n\n");
+	check_ft_strcpy("a\0b\0c\0");
+	check_ft_strcpy("!@#$%^&*()_+|~QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>?");
+	check_ft_strcpy(":\"A");
+	check_ft_strcpy("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+	check_ft_strcpy("!@#$%^&*()_+|~\"QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>?");
+	check_ft_strcpy("!@#$%^&*()_+|~\"QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?");
+	check_ft_strcpy("\'\'\'\'\'\'\'\'");
+	check_ft_strcpy("\"\"\"\"\"\"\"\"");
+	s = long_char(100, 'A');
+	check_ft_strcpy(s);
+	free(s);
+	write(1, "\n", 1);
+}
+
+void	check_ft_strlen(char *s)
+{
+	if (strlen(s) == ft_strlen(s))
+		printf("%sOK%s [%s]\n", GREEN, RESET, s);
+	else
+		printf("%sNG%s [%s]\n", RED, RESET, s);
+}
+
 void	test_ft_strlen(void)
 {
 	char	*s;
@@ -267,25 +265,13 @@ void	test_ft_strlen(void)
 	write(1, "\n", 1);
 }
 
-void	check_ft_strlen(char *s)
+int		main(void)
 {
-	if (strlen(s) == ft_strlen(s))
-		printf("%sOK%s [%s]\n", GREEN, RESET, s);
-	else
-		printf("%sNG%s [%s]\n", RED, RESET, s);
-}
-
-char	*long_char(size_t size, char c)
-{
-	char	*s;
-
-	if ((s = malloc(size)) == NULL)
-	{
-		dprintf(2, "malloc failed\n");
-		exit(1);
-	}
-	s[--size] = '\0';
-	while (--size)
-		s[size] = c;
-	return (s);
+	test_ft_strlen();
+	test_ft_strcpy();
+	test_ft_strcmp();
+	test_ft_write();
+	test_ft_read();
+	test_ft_strdup();
+	return (0);
 }
